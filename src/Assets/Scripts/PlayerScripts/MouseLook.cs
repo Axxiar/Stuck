f71581c2022e1ffe92401cbad50388a586243828
Axxiar <galoualexis@gmail.com>
2023-03-01 19:12:30 +0100

    basic player and mouse movements

--- src/Assets/Scripts/PlayerScripts/MouseLook.cs
+++ src/Assets/Scripts/PlayerScripts/MouseLook.cs
@@ -6,12 +6,19 @@ public class MouseLook : MonoBehaviour
     // Propriété transform du joueur dont c'est la caméra
     public Transform playerBody;
     // Sensibilité verticale de la souris
-    public float mouseYSensivity = 350.0f;
+    public float mouseYSensivity = 100.0f;
     // Sensibilité horizontale de la souris
-    public float mouseXSensivity = 300.0f;
+    public float mouseXSensivity = 70.0f;
+    // 
+    [Range(0.0f, 0.3f)] public float mouseSmoothness = 0.07f;
+    
     // Valeur de la rotation sur l'axe X (haut en bas)
     private float _xRotation;
+    //
+    Vector2 _currentMouseDelta = Vector2.zero;
+    Vector2 _currentMouseDeltaVelocity = Vector2.zero;
 
+    
     void Start()
     {
         // On bloque le curseur (+rend invisible au passage)
@@ -21,16 +28,18 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         // Récup de la position de la souris
-        float mouseX = Input.GetAxis("Mouse X") * mouseXSensivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseYSensivity * Time.deltaTime;
-
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        
+        // smooth de la souris
+        _currentMouseDelta = Vector2.SmoothDamp(_currentMouseDelta, targetMouseDelta, ref _currentMouseDeltaVelocity, mouseSmoothness);
+        
         // update de la rotation verticale (-= car l'axe est inversé par défaut)
-        _xRotation -= mouseY;
+        _xRotation -= _currentMouseDelta.y;
         // bloque la rotation verticale entre -90 et 90
         _xRotation = Math.Clamp(_xRotation, -90f, 90f);
         
         // update des rotations
         transform.localRotation = Quaternion.Euler(_xRotation, 0f ,0f); // ici on fait tourner la caméra
-        playerBody.Rotate(Vector3.up * mouseX); // ici le joueur directement (quand il regarde à gauche/droite, ça fait tourner son corps directement)
+        playerBody.Rotate(Vector3.up * _currentMouseDelta.x); // ici le joueur directement (quand il regarde à gauche/droite, ça fait tourner son corps directement)
     }
 }
--- src/Assets/Scripts/PlayerScripts/Movements.cs
+++ src/Assets/Scripts/PlayerScripts/Movements.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -6,33 +7,62 @@ public class Movements : MonoBehaviour
     // controller du charcter du joueur;
     public CharacterController controller;
 
+    // Vitesse de marche lente
+    public float slowWalkSpeed = 3.0f;
     // Vitesse de marche
-    public float walkingSpeed = 10f;
-
+    public float walkSpeed = 7f;
     // Vitesse de course
-    public float runningSpeed = 15f;
+    public float runSpeed = 15f;
+    // Hauteur de saut
+    public float jumpHeight = 8f;
+    // Gravité
+    // Cette valeur DOIT être négative sinon le joueur "tombe" vers le ciel
+    public float gravity = -19.62f;
+    
+    // Fluidité du mouvement
+    [Range(0.0f, 0.4f)] public float moveSmoothness = 0.1f;
 
-    //Vitesse de saut
-    public float jumpSpeed = 8f;
-
-    //Gravité
-    public float gravity = 20f;
-
-    // Déplacement
-    Vector3 _moveDirection;
-
-    // booléen marche ou court ?
-    bool _isRunning;
-
+    Vector2 _currentDir = Vector2.zero;
+    Vector2 _currentVelocity = Vector2.zero;
+    float _velocityY = 0.0f;
+    float _currentSpeed;
+    
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
+        // gestion des différentes vitesses de déplacement
+        //fast
+        if (Input.GetKey(KeyCode.LeftShift))
+            _currentSpeed = runSpeed;
+        //slow
+        else if (Input.GetKey(KeyCode.LeftControl))
+            _currentSpeed = slowWalkSpeed;
+        //normal
+        else
+            _currentSpeed = walkSpeed;
+        
         // récup des directions
-        Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         
         // normalisation du vecteur pour que sa longueur soit toujours 1 même en diagonal
-        inputDir.Normalize();
-        
-        
+        targetDir.Normalize();
+
+        // smooth du déplacement (uniquement axe z et x)
+        _currentDir = Vector2.SmoothDamp(_currentDir, targetDir, ref _currentVelocity, moveSmoothness);
+
+        // si saut
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+            _velocityY = jumpHeight;
+        // sinon on chute
+        else
+            _velocityY += gravity * Time.deltaTime;
+
+        // application des mouvement sur tous les axes : x y z
+        Vector3 velocity = (transform.forward * _currentDir.y + transform.right * _currentDir.x) * _currentSpeed + Vector3.up * _velocityY;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
