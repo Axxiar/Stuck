e2f4f8b28b34329bd8ae0c0bd40c54ae12552a2c
Axxiar <galoualexis@gmail.com>
2023-03-06 19:01:50 +0100

    whistle (not synch yet)

---
+++ src/Assets/Scripts/PlayerScripts/Whistle.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Whistle : MonoBehaviour
+{
+    public AudioClip[] whistleClips;
+    public float whistleCoolDown = 10f;
+
+    private AudioSource audioSource;
+    private float currentWhistleCD;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !audioSource.isPlaying && currentWhistleCD <= 0)
+        {
+            int randomIndex = Random.Range(0, whistleClips.Length);
+            audioSource.PlayOneShot(whistleClips[randomIndex]);
+            currentWhistleCD = whistleCoolDown;
+        }
+
+        if (currentWhistleCD > 0)
+            currentWhistleCD -= Time.deltaTime;
+    }
+}
