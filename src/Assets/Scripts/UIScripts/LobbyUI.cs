93698eae1c027911b3677937a65d345dd3ca7fd4
Axxiar <galoualexis@gmail.com>
2023-04-13 19:44:34 +0200

    ui more responsive*

---
+++ src/Assets/Scripts/UIScripts/LobbyUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyUI : MonoBehaviour
+{
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
