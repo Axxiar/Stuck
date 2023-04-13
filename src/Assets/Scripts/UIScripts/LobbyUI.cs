0df25e12f97ceee2bdf0ccb2de91599457ce75ef
Axxiar <galoualexis@gmail.com>
2023-04-13 23:39:52 +0200

    added a login scene
    lisez les infos sur ce qu'il faudrait (au moins) ajouter. Elle sont écrites dans l'ui de la LoginScene

--- src/Assets/Scripts/UIScripts/LobbyUI.cs
+++ src/Assets/Scripts/UIScripts/LobbyUI.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LobbyUI : MonoBehaviour
 {
+    public void StartGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
     public void QuitGame ()
     {
         Application.Quit();
