22c4681d8d29c817dbacb32e82c852f62b516db4
Axxiar <galoualexis@gmail.com>
2023-04-19 03:34:20 +0200

    funny easter egg on website

---
+++ docs/js/card.js
@@ -0,0 +1,11 @@
+const says = ["Stop that","Stop punching me plz","Hey !! It hurts !","what ?","go away","😴","Zzz.. Hm ?","Zzz"];
+
+window.addEventListener("load", function(){
+    var cards = document.querySelectorAll(".card");
+    
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function() {
+            alert(says[Math.floor(Math.random() * says.length)])
+        });
+    }
+});
