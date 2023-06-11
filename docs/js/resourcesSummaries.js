7961e09de01097b842f2f21d2a1caa38d08eda50
Axxiar <galoualexis@gmail.com>
2023-06-11 16:08:41 +0200

    lils improvements

---
+++ docs/js/resourcesSummaries.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var details = document.querySelectorAll("details")
+    for (let i = 0; i < details.length; i++) {
+        details[i].querySelector("summary").addEventListener("click", function(){
+            if (details[i].classList == "closed") {
+                details[i].classList = "opened"
+            } else {
+                details[i].classList = "closed"
+            }
+        })
+    }
+}
